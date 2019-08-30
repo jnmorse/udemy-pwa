119c3fd58781439faab72cb0bc2f4ff29ec5cef4
Joseph Morse <tamed.lionheart@gmail.com>
2019-08-29 20:11:39 -0400

    chore: Second Project Setup

---
+++ course-project-starting-setup/public/src/js/feed.js
@@ -0,0 +1,17 @@
+const shareImageButton = document.querySelector('#share-image-button');
+const createPostArea = document.querySelector('#create-post');
+const closeCreatePostModalButton = document.querySelector(
+  '#close-create-post-modal-btn'
+);
+
+function openCreatePostModal() {
+  createPostArea.style.display = 'block';
+}
+
+function closeCreatePostModal() {
+  createPostArea.style.display = 'none';
+}
+
+shareImageButton.addEventListener('click', openCreatePostModal);
+
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
