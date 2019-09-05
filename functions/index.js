42475e34a1a8a2c416efdee955a88660b59959e6
Joseph Morse <tamed.lionheart@gmail.com>
2019-09-05 09:28:22 -0400

    updated email to mine again

--- functions/index.js
+++ functions/index.js
@@ -31,7 +31,7 @@ exports.storePostData = functions.https.onRequest((request, response) => {
       })
       .then(() => {
         webpush.setVapidDetails(
-          'mailto:business@academind.com',
+          'mailto:tamed.lionheart@gmail.com',
           functions.config().webpush.public,
           functions.config().webpush.private
         );
